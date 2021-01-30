ea1ed5e9ec82d12f08e9b1e9f81c41c8896056f9
Ivan Galarza <id.galarza.sa@gmail.com>
2021-01-30 11:31:59 -0400

    Basic struct

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:components_list/src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Components List',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
